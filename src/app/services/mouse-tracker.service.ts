966372b88390a476d4be926a23a4cd21ec460973
temarigod <baranyaimark17@gmail.com>
2022-05-05 07:06:53 +0200

    optimization done

--- src/app/services/mouse-tracker.service.ts
+++ src/app/services/mouse-tracker.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 import { BehaviorSubject, fromEvent } from 'rxjs';
 
 @Injectable({
@@ -9,17 +9,21 @@ export class MouseTrackerService {
 
   public mouseUpperLeftCorner$ = this.onTheLeftUpperSideSubject$.asObservable();
 
-  constructor() {}
+  constructor(private readonly ngZone: NgZone) {}
 
   public start() {
-    fromEvent<MouseEvent>(document.body, 'mousemove', {
-      capture: true,
-    }).subscribe((e) => {
-      let oldValue = this.onTheLeftUpperSideSubject$.getValue();
-      const newValue = e.clientX < 100 && e.clientY < 100;
-      if (newValue !== oldValue) {
-        this.onTheLeftUpperSideSubject$.next(newValue);
-      }
+    this.ngZone.runOutsideAngular(() => {
+      fromEvent<MouseEvent>(document.body, 'mousemove', {
+        capture: true,
+      }).subscribe((e) => {
+        let oldValue = this.onTheLeftUpperSideSubject$.getValue();
+        const newValue = e.clientX < 100 && e.clientY < 100;
+        if (newValue !== oldValue) {
+          this.ngZone.run(() => {
+            this.onTheLeftUpperSideSubject$.next(newValue);
+          });
+        }
+      });
     });
   }
 }
